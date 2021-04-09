ecfcd23331a7fba9168a2dfc224c1331f7360130
jorgeebn16 <jorgeebn16@gmail.com>
2021-04-08 23:43:50 -0400

    React app Setup & Redux Install
    React app Setup & Redux Install

---
+++ client/src/auth/Register.js
@@ -0,0 +1,9 @@
+const Register = () => {
+    return (
+        <div className="conatiner-fluid h1 p-5 text-center">
+            Registration Page
+        </div>
+    )
+}
+
+export default Register;
