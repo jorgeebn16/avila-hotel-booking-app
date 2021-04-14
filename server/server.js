c98ef0aec643860c119ef8d857c2769dc1e8b579
jorgeebn16 <jorgeebn16@gmail.com>
2021-04-14 19:47:37 -0400

    server
    server

--- server/server.js
+++ server/server.js
@@ -11,17 +11,22 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
-app.use(cors());
-app.use(morgan("dev"));
-app.use(express.json());
-
-// route middleware
 readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
 
 const port = process.env.PORT || 8000;
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
+app.use('/images', express.static(path.join(__dirname, '../client/images')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(port, () => console.log(`Server is running on port ${port}`));
