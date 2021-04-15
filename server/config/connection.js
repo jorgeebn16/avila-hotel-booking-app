b38f16839357e21786b68d0266722419688e5413
jorgeebn16 <jorgeebn16@gmail.com>
2021-04-15 00:50:31 -0400

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,10 +2,9 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/avila-hotel',  {
     useNewUrlParser: true,
-    useFindAndModify: false,
     useUnifiedTopology: true,
     useCreateIndex: true,
-
-})
-
-module.exports = mongoose.connection;
+    useFindAndModify: true,
+  });
+  
+  module.exports = mongoose.connection;
