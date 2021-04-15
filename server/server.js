959eb47409e2f2a98182c4c4fa5d338d559e26fe
jorgeebn16 <jorgeebn16@gmail.com>
2021-04-14 21:21:57 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,11 +6,26 @@ const morgan = require("morgan");
 const db = require('./config/connection');
 require("dotenv").config();
 const app = express();
+const path = require('path');
 
 // middlewares
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Serve up static assets
+app.use('/images', express.static(path.join(__dirname, '../client/images')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 // route middleware
 readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
