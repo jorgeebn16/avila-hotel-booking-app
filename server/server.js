81d778818520a709b89c00f0ee07fab63552bdfb
jorgeebn16 <jorgeebn16@gmail.com>
2021-04-14 20:02:48 -0400

    Server
    Server

--- server/server.js
+++ server/server.js
@@ -4,29 +4,18 @@ import cors from "cors";
 const morgan = require("morgan");
 require("dotenv").config();
 const db = require('./config/connection');
-const path = require('path');
+
 const app = express();
 
+// middlewares
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
+// route middleware
 readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
 
 const port = process.env.PORT || 8000;
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-app.use('/images', express.static(path.join(__dirname, '../client/images')));
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
 
 db.once('open', () => {
   app.listen(port, () => console.log(`Server is running on port ${port}`));
